c61555fcebe658f65f401e30d2e18d83b7b91833
achornocyam <maycon.alex.rocha@hotmail.com>
2015-03-08 21:02:46 -0300

    funcao maior idade

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <?php
 
-phpinfo();
+header('Content-type:text/html;charset=utf-8');
+
+$maiorIdade = function ($idade){
+  echo ($idade > 18)? "Você é maior de idade ": "Você é menor de idade";  
+};
+
+/* @var $maiorIdade int*/
+$maiorIdade(18);
