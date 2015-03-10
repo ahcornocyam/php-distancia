24d6c281aea090a32a381c26befb398b139214f8
achornocyam <maycon.alex.rocha@hotmail.com>
2015-03-09 21:11:35 -0300

    brincando com php de forma simples

--- src/verificaidade.php
+++ src/verificaidade.php
@@ -8,14 +8,21 @@
 
 header('Content-type:text/html;charset=utf-8');
 /* @var $nome type string */
-$nome  = filter_input(INPUT_POST, 'inputNome',  FILTER_SANITIZE_STRING);
+$nome  = \ucfirst(\strtolower(filter_input(INPUT_POST, 'inputNome', FILTER_SANITIZE_STRING)));
 /* @var $idade type int */
 $idade = filter_input(INPUT_POST, 'inputIdade', FILTER_VALIDATE_INT);
 
+/**
+ * @param type $nome parametro para o nome
+ * @param type $idade parametro para a idade
+ * @return type String retorna se a pessoa é maior ou menor de idade
+ */
 $maiorIdade = function ($nome,$idade){
-  echo ($idade > 18)? "{$nome}, você é maior de idade ": "{$nome}, você é menor de idade";  
+    
+    return  ($idade > 18)? "{$nome}, você é maior de idade ": "{$nome}, você é menor de idade"; 
+  
 };
 
 #chamando a função maiorIdade
 /* @var $maiorIdade type function */
-$maiorIdade($nome , $idade);
+echo $maiorIdade($nome , $idade);
